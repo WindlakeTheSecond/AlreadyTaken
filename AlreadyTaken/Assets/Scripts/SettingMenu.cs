5fa63082a89a8fb2e21728d91dd1442ff7b114b1
AkosMolnar <akoska0809@gmail.com>
2022-05-14 19:12:49 +0200

    Menu
    Opiton bővítése

---
+++ AlreadyTaken/Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("MasterVolume", volume);
+
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
